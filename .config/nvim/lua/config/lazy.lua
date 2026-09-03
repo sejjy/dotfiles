8b5b059fa53fe9298655c200d3e9ba5706d7a493
sejjy <sejjymvm@gmail.com>
2026-09-03 10:23:41 +0800

    cleanup

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -1,7 +1,12 @@
--- This is crazy
-vim.pack.add({
-	{ src = "https://github.com/folke/lazy.nvim", version = "stable" },
-})
+-- Bootstrap lazy and all plugins
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.uv.fs_stat(lazypath) then
+	local repo = "https://github.com/folke/lazy.nvim.git"
+	vim.fn.system({ "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath })
+end
+
+vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
 	spec = {
