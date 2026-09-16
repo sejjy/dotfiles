d48628197c6e957920963e5ae28694c56a30d111
sejjy <sejjymvm@gmail.com>
2026-09-16 12:30:01 +0800

    nvim: update c hl

--- .config/nvim/lua/plugins/github-theme.lua
+++ .config/nvim/lua/plugins/github-theme.lua
@@ -21,13 +21,15 @@ return {
 		vim.api.nvim_set_hl(0, "@variable.builtin.bash",  { link = "@variable" })
 
 		-- C
-		vim.api.nvim_set_hl(0, "@boolean.c",          { link = "@variable" })
-		vim.api.nvim_set_hl(0, "@character.c",        { link = "@number.c" })
-		vim.api.nvim_set_hl(0, "@constant.macro.c",   { link = "@number.c" })
-		vim.api.nvim_set_hl(0, "@keyword.operator.c", { link = "@variable" })
-		vim.api.nvim_set_hl(0, "@lsp.type.macro.c",   {})
-		vim.api.nvim_set_hl(0, "@string.escape.c",    { bold = false })
-		vim.api.nvim_set_hl(0, "@type.builtin.c",     { link = "@variable" })
+		vim.api.nvim_set_hl(0, "@boolean.c",           { link = "@variable" })
+		vim.api.nvim_set_hl(0, "@character.c",         { link = "@number.c" })
+		vim.api.nvim_set_hl(0, "@constant.macro.c",    { link = "@number.c" })
+		vim.api.nvim_set_hl(0, "@keyword.operator.c",  { link = "@variable" })
+		vim.api.nvim_set_hl(0, "@lsp.type.macro.c",    {})
+		vim.api.nvim_set_hl(0, "@lsp.type.operator.c", { link = "@variable" })
+		vim.api.nvim_set_hl(0, "@operator.c",          { link = "@variable" })
+		vim.api.nvim_set_hl(0, "@string.escape.c",     { bold = false })
+		vim.api.nvim_set_hl(0, "@type.builtin.c",      { link = "@variable" })
 
 		local float = vim.api.nvim_get_hl(0, { name = "NormalFloat" })
 
