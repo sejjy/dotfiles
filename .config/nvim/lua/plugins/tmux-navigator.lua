940d95f6f6bdaeb1d8e9364160e5e6bb31f60bbd
sejjy <sejjymvm@gmail.com>
2026-09-17 13:35:43 +0800

    tmux config fixes

--- .config/nvim/lua/plugins/tmux-navigator.lua
+++ .config/nvim/lua/plugins/tmux-navigator.lua
@@ -1,6 +1,15 @@
 return {
 	"christoomey/vim-tmux-navigator",
 
+	cmd = {
+		"TmuxNavigateLeft",
+		"TmuxNavigateDown",
+		"TmuxNavigateUp",
+		"TmuxNavigateRight",
+		"TmuxNavigatePrevious",
+		"TmuxNavigatorProcessList",
+	},
+
 	keys = {
 		{ "<C-h>",  "<Cmd><C-u>TmuxNavigateLeft<Enter>",     desc = "Tmux navigate left" },
 		{ "<C-j>",  "<Cmd><C-u>TmuxNavigateDown<Enter>",     desc = "Tmux navigate down" },
